af3240ad401721213ab4eff8e5360e07c249ee92
erinflynn <eflynn3@nd.edu>
2017-05-05 16:45:55 -0400

    fized bakcground

--- game.py
+++ game.py
@@ -5,23 +5,35 @@ import math
 class GameSpace:
     def main(self):
         pygame.init()
-        self.size = self.width, self.height = 640,480
+
+        oceanImage = pygame.image.load("oceanBackground.png")
+        self.size = self.width, self.height = 800,600
         self.black = 0,0,0
         self.screen = pygame.display.set_mode(self.size)
-        #instantiate objects
-
-        self.clock = pygame.tick.Clock()
-        pygame.key.set_repeat(1, 10)
-        while(1):
-            self.clock.tick(60)
+        self.screen.blit(oceanImage, [0,0])
+        done = False
+        while not done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.black)
-            #blit
+                        done = True
             pygame.display.flip()
+        pygame.display.set_caption("Fish Fun")
+        self.screen.fill(self.black)
 
+        #instantiate objects
+
+        #self.clock = pygame.tick.Clock()
+        #pygame.key.set_repeat(1, 10)
+        #while(1):
+        #    self.clock.tick(60)
+        #    for event in pygame.event.get():
+        #        if event.type == pygame.QUIT:
+        #            sys.exit()
+
+        #    self.screen.fill(self.black)
+            #blit
+        #    pygame.display.flip()
+        #pygame.blit(oceanImage, (0,0))
 if __name__ == "__main__":
     gs = GameSpace()
     gs.main()
