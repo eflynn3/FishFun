54c59a8d2aeb289ac1888e91ff29df1e77b18533
erinflynn <eflynn3@nd.edu>
2017-05-05 22:16:04 -0400

    somewhat moving fish

--- blueFish.py
+++ blueFish.py
@@ -1,15 +1,22 @@
+import pygame
+
 class blueFish(pygame.sprite.Sprite):
     def __init__(self, gs, ycoord, direction):
-        self.image = pygame.image.load()
-        self.rect = self.image.get_rect()
+        self.image = pygame.image.load("blueFish.png")
+        self.image = pygame.transform.scale(self.image, (60, 60)) 
+        self.rect = self.image.get_rect(center = (50, 50))
+
         self.direction = direction
         self.rect.y = ycoord
         if self.direction == "left":
             self.rect.x = 640
         else:
             self.rect.x = 0
-    def move(self):
+    
+    def tick(self):
         if(self.direction == "left"):
             self.rect.x -= 1
         else:
-            self.rect.x += 1
+            self.rect.x += 1  
+
+        
--- game.py
+++ game.py
@@ -1,24 +1,35 @@
 import sys
 import pygame
 import math
+from blueFish import blueFish
 
 class GameSpace:
     def main(self):
+        #initialize gamespace
         pygame.init()
-
+        self.clock = pygame.time.Clock()
         oceanImage = pygame.image.load("oceanBackground.png")
-        self.size = self.width, self.height = 800,600
-        self.black = 0,0,0
+        self.size = self.width, self.height = 800, 600
         self.screen = pygame.display.set_mode(self.size)
         self.screen.blit(oceanImage, [0,0])
+
+        # initialize all game objects
+        self.blueFish = blueFish(self, 50, "left")
+
         done = False
         while not done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
+
+        # call tick on each item 
+            self.blueFish.tick()
+        #Output to screen 
+            self.screen.blit(self.blueFish.image, self.blueFish.rect)
             pygame.display.flip()
-        pygame.display.set_caption("Fish Fun")
-        self.screen.fill(self.black)
+
+        #game.display.set_caption("Fish Fun")
+        #self.screen.fill(self.black)
 
         #instantiate objects
 
@@ -34,6 +45,7 @@ class GameSpace:
             #blit
         #    pygame.display.flip()
         #pygame.blit(oceanImage, (0,0))
+
 if __name__ == "__main__":
     gs = GameSpace()
     gs.main()
