94465f82cf2b634ec86f018f62089b1ecf85fcab
Erin Turley <eturley@nd.edu>
2017-05-07 13:38:46 -0400

    start of points

--- game.py
+++ game.py
@@ -11,10 +11,10 @@ class GameSpace:
         #initialize gamespace
         pygame.init()
         self.clock = pygame.time.Clock()
-        oceanImage = pygame.image.load("oceanBackground.png")
+        self.oceanImage = pygame.image.load("oceanBackground.png")
         self.size = self.width, self.height = 800, 600
         self.screen = pygame.display.set_mode(self.size)
-        self.screen.blit(oceanImage, [0,0])
+        self.screen.blit(self.oceanImage, [0,0])
 
         self.fishes = pygame.sprite.Group()
         # initialize all game objects
@@ -22,9 +22,11 @@ class GameSpace:
         self.pinkFish = enemyFish(self, 20, "right", "pinkFish.png", 15)
         self.blueFish = enemyFish(self, 50, "left", "blueFish.png", 60)
         self.greenFish = enemyFish(self, 150, "right", "greenFish.png", 30)
+        self.goldFish = enemyFish(self, 200, "left", "goldFish.png", 120)
         self.fishes.add(self.pinkFish)
         self.fishes.add(self.blueFish)
         self.fishes.add(self.greenFish)
+        self.fishes.add(self.goldFish)
         
         done = False
         pygame.key.set_repeat(1, 10)
@@ -48,8 +50,12 @@ class GameSpace:
 #            self.blueFish.tick()
 #            self.greenFish.tick()
 #            self.pinkFish.tick()
-            self.screen.blit(oceanImage, (0,0))
+            self.screen.blit(self.oceanImage, (0,0))
             self.fishes.draw(self.screen)
+            #points
+            text = pygame.font.SysFont("monospace", 20)
+            label = text.render(self.playerFish.points, 1, (255, 255, 0))
+            self.screen.blit(label, (50, 50))
 #            self.screen.blit(self.pinkFish.image, self.pinkFish.rect)
 #            self.screen.blit(self.blueFish.image, self.blueFish.rect)
 #            self.screen.blit(self.greenFish.image, self.greenFish.rect)
@@ -60,6 +66,12 @@ class GameSpace:
         #self.screen.fill(self.black)
 
     def end_game(self):
+        self.screen.blit(self.oceanImage, (0,0))
+        mytext = pygame.font.SysFont("monospace", 20)
+        label = mytext.render("You Lost!", 1, (255, 255, 0))
+        self.screen.blit(label, (100, 100))
+        pygame.display.flip()
+        pygame.time.wait(5000)
         sys.exit()
 
 if __name__ == "__main__":
--- playerFish.py
+++ playerFish.py
@@ -8,10 +8,15 @@ class playerFish(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 50
         self.rect.y = 50
+        self.points = 0
 
     def move(self, xc, yc):
         self.rect.x += xc
         self.rect.y += yc
+
+    def add_points(self, points):
+        self.points += points
+    
     def change_size(self):
         self.size += 5
         #self.image = pygame.transform.scale(self.image, (50, 50))
